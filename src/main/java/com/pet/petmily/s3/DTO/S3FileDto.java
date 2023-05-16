0ed9c239fa40b5b5ca6e2af5b603fa8bc163e2f9
PRsH1 <impjs17@gmail.com>
2023-05-16 16:50:45 +0900

    14(add):이미지 업로드 초기 구현(코드 리팩토링 많이 필요함) 업로드 테스트는 성공

---
+++ src/main/java/com/pet/petmily/s3/DTO/S3FileDto.java
@@ -0,0 +1,19 @@
+package com.pet.petmily.s3.DTO;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class S3FileDto {
+
+    private String originalFileName;
+    private String uploadFileName;
+    private String uploadFilePath;
+    private String uploadFileUrl;
+}
