456241639f3f5d9efd94250ec85bca18fbb142db
PRsH1 <impjs17@gmail.com>
2023-05-17 21:57:36 +0900

    15(add):다중 업로드 기능 추가, Response 형식 추가, Response 데이터 가독성 향상 업데이트,계속 리팩토링 중

---
+++ src/main/java/com/pet/petmily/s3/service/FileUploadService.java
@@ -0,0 +1,63 @@
+package com.pet.petmily.s3.service;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FileUploadService {
+
+    private final UploadService s3Service;
+
+    public List<String> uploadImages(MultipartFile[] files) {
+        List<String> uploadedFileUrls = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            String fileName = createFileName(file.getOriginalFilename());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try (InputStream inputStream = file.getInputStream()) {
+                s3Service.uploadFile(inputStream, objectMetadata, fileName);
+                String fileUrl = s3Service.getFileUrl(fileName);
+                uploadedFileUrls.add(fileUrl);
+            } catch (IOException e) {
+                throw new IllegalArgumentException(String.format("파일 변환 에러 (%s)", file.getOriginalFilename()));
+            }
+        }
+
+        return uploadedFileUrls;
+    }
+
+    // 기존 확장자명을 유지한 채, 유니크한 파일의 이름을 생성하는 로직
+    private String createFileName(String originalFileName) {
+        log.info("createFileName() 호출");
+        String folderName = "pet";
+        return folderName+"/"+UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    // 파일의 확장자명을 가져오는 로직
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+
+    public byte[] downloadImage(String fileName) {
+        log.info("downloadImage() 호출");
+        return s3Service.downloadFile(fileName);
+    }
+}
