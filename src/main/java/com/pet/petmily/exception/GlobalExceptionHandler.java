29b72e73cdf35c85f25cca8fa61935b3ea4009f3
PRsH1 <impjs17@gmail.com>
2023-06-02 16:41:27 +0900

    26(add):ResponseStatusException 예외처리에 대한 글로벌 설정

---
+++ src/main/java/com/pet/petmily/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.pet.petmily.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getStatus().value(), ex.getReason());
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    }
+}
